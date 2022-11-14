2e5f9cdc2f71acf93171f0de5fe0bbada700f839
Emmany555 <110328528+Emmany555@users.noreply.github.com>
2022-11-14 23:54:57 +0100

    Add files via upload
    new commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// document.readyState
+
+$("h1").mouseover(function(){
+  // console.log(event.key)
+  $('h1').animate({margin: '4rem'});
+});
+// $("h1").mouseout(function(){
+//   $('h1').slideDown();
+
+// });
+
+
+
